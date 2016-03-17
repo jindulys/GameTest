0c71ef7fa5681a4af64995e27dd79c816e6579a5
jindulys <shadowy1810@gmail.com>
2016-03-17 00:43:10 -0400

    Another local changed

--- GameTest/ViewController.m
+++ GameTest/ViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"Test input");
     NSLog(@"Another test");
+    NSLog(@"Fuck LYS");
 }
 
 - (void)didReceiveMemoryWarning {
